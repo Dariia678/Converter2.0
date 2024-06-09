21f0a7298af1387d3ac98c6c8528361d9e27a0f7
Makarenko Dariia <vermicell.i678@gmail.com>
2024-06-09 23:00:38 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Project Converter: This project converts " +
+                "miles to kilometers and vice versa.");
+    }
+}
