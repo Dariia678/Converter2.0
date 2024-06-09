7589b227cadb4511181d51b59781a254f0a1aab2
Makarenko Dariia <vermicell.i678@gmail.com>
2024-06-09 23:14:44 +0200

    Added kilometers to miles conversion functionality

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,8 +7,13 @@ public class Main {
         System.out.println("Project Converter: This project converts " +
                 "miles to kilometers and vice versa.");
         System.out.println("5 miles is " + milesToKilometers(5));
+        System.out.println("8 kilometers is " + kilometersToMiles(8) + " miles.");
     }
     public static double milesToKilometers(double miles) {
-        return miles *1.56849;
+        return miles * 1.56849;
+    }
+
+    public static double kilometersToMiles(double kilometers) {
+        return kilometers / 1.56849;
     }
 }
